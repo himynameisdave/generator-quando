cfa7ebb562fb002470c4917573022541394ba311
dlunny <dlunny@featurecreeptechnology.com>
2015-10-20 20:33:17 -0400

    Updates the original webpack.config

--- generators/app/templates/_webpack.config.js
+++ generators/app/templates/_webpack.config.js
@@ -1,43 +1,28 @@
-var path = require('path');
-var webpack = require('webpack');
+var commonConfig      = require("./webpack.common.config.js"),
+    HtmlWebpackPlugin = require("html-webpack-plugin"),
+    webpack           = require("webpack");
 
 module.exports = {
-  devtool: 'eval',
-
   entry: [
-    'webpack-dev-server/client?http://localhost:8080',
-    'webpack/hot/only-dev-server',
-    './src/js/index'
+  './src/js/index.js'
   ],
-
   output: {
-    path: path.join(__dirname, 'dist', 'js'),
-    filename: 'index.js',
-    publicPath: '/static/js/'
+    path: './dist',
+    filename: 'bundle.[hash].js'
   },
-
-  plugins: [
-    new webpack.HotModuleReplacementPlugin()
-  ],
-
+  devtool:'source-map',
+  devServer: {
+    // proxy calls to api to our own node server backend
+    proxy: {
+      '/api/*': 'http://localhost:5000/'
+    }
+  },
   module: {
-    loaders: [{
-      test: /\.js$/,
-      exclude: /node_modules/,
-      loaders: ['react-hot', 'babel'],
-      include: path.join(__dirname, 'src')
-    },{
-      test: /\.png$/,
-      loader: "url-loader?limit=100000"
-    },{
-      test: /\.jpg$/,
-      loader: "file-loader"
-    },{
-      test: /\.css$/,
-      loader: "style!css"
-    },{
-      test: /\.less$/,
-      loader: "css!less"
-    }]
-  }
-};
+    loaders: commonConfig.loaders
+  },
+  plugins: [
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.UglifyJsPlugin({minimize: true}),
+    commonConfig.indexPagePlugin
+  ],
+}
