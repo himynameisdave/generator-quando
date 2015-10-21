d6e2b67b1661539db548526ef689f82ee79c0421
dlunny <dlunny@featurecreeptechnology.com>
2015-10-20 20:30:05 -0400

    Updates the server & package.json

--- generators/app/templates/_server.js
+++ generators/app/templates/_server.js
@@ -1,15 +1,45 @@
-var webpack          = require('webpack');
-var WebpackDevServer = require('webpack-dev-server');
-var config           = require('./webpack.config');
+var express     = require('express'),
+    compression = require('compression'),
+    path        = require('path'),
+    cors        = require('cors'),
+    app         = express(),
+    static_path = path.join(__dirname, './dist');
 
-new WebpackDevServer(webpack(config), {
-  contentBase: './dist',
-  publicPath: config.output.publicPath,
-  hot: true,
-  historyApiFallback: true
-}).listen(8080, 'localhost', function (err, result) {
-  if (err) {
-    console.log(err);
-  }
-  console.log('Listening at localhost:8080');
+//  trust proxies
+app.enable('trust proxy');
+//  enable gzipping
+app.use(compression());
+
+app.options('/api/currentTime', cors());
+app.get('/api/currentTime', cors(), function(req, res) {
+  res.send({ time: new Date() });
+});
+
+//  BLACK MAGIC
+app.route('/').get(function(req, res) {
+    res.header('Cache-Control', "max-age=60, must-revalidate, private");
+    res.sendFile('index.html', {
+        root: static_path
+    });
+});
+//  ALSO BLACK MAGIC
+function nocache(req, res, next) {
+  res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
+  res.header('Expires', '-1');
+  res.header('Pragma', 'no-cache');
+  next();
+}
+
+app.use('/', express.static(static_path, {
+    maxage: 31557600
+}));
+
+
+var server = app.listen(process.env.PORT || 5000, function () {
+
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+
 });
